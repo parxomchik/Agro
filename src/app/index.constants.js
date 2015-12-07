7847540a7cfb9d470761d0119d34f127008cb48b
parkhomenko <dmitriy.parkhomenko@icloud.com>
2015-12-07 14:27:00 +0200

    init comit

---
+++ src/app/index.constants.js
@@ -0,0 +1,11 @@
+/* global malarkey:false, toastr:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('agro')
+    .constant('malarkey', malarkey)
+    .constant('toastr', toastr)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('agro', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngResource', 'ui.router', 'mgcrea.ngStrap']);
+
+})();
